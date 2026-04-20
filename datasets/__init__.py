1f28eabf8072d05c725845df8caddedf9b0b3bcb
Yuxi liu <111115237+Yuxi104@users.noreply.github.com>
2026-04-20 11:57:01 +0800

    Add files via upload

---
+++ datasets/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from .cityscapes import Cityscapes as cityscapes
+from .camvid import CamVid as camvid
+from .bdd100k import BDD as bdd100k
